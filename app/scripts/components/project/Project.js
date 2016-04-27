eca801e446b5fbc4f91c5b7cbd90c15734c2706f
Brossault <brossault.leo@gmail.com>
2016-04-27 09:16:58 +0200

    next project

--- app/scripts/components/project/Project.js
+++ app/scripts/components/project/Project.js
@@ -26,6 +26,7 @@ let Project = Vue.extend({
       container[0].classList.add('project-page');
 
       window.addEventListener('scroll', this.isInViewPort);
+      console.log(this.$parent);
     },
     methods: {
       isInViewPort () {
@@ -41,6 +42,63 @@ let Project = Vue.extend({
             item.classList.add('isIt');
           }
         });
+      },
+      nextProject () {
+        let scrollTop = document.body.scrollTop,
+            viewportHeight = window.innerHeight,
+            footer = document.querySelectorAll('.next-project')[0],
+            home = document.querySelectorAll('.home')[0],
+            top = document.body.scrollTop + footer.getBoundingClientRect().top,
+            sum = top - scrollTop + viewportHeight,
+            project = document.querySelectorAll('.project')[0],
+            link = document.querySelectorAll('.next-project a')[0].getAttribute('data-link'),
+            _this = this,
+            diff;
+
+        diff = 200 - (scrollTop + viewportHeight - top);
+
+        home.parentNode.insertBefore(footer, home.nextSibling);
+        footer.classList.add('go-to-next');
+        project.classList.add('go-to-next');
+        footer.style.height = window.innerHeight + 'px';
+        footer.style.bottom = -diff + 'px';
+
+        setTimeout(function () {
+          footer.style.maxHeight = window.innerHeight + 'px';
+          footer.style.transform = 'translateY(-'+ diff +'px)';
+        }, 600);
+
+        setTimeout(function () {
+          scrollTo(0, 0);
+          _this.$parent.nextProject();
+          _this.initNextProject(link);
+        }, 1200);
+      },
+      initNextProject (link) {
+        let prepareFooter = document.querySelectorAll('.next-project-prepare')[0],
+            home = document.querySelectorAll('.home')[0],
+            project = document.querySelectorAll('.project')[0],
+            footer = document.querySelectorAll('.next-project')[0];
+
+        home.parentNode.insertBefore(prepareFooter, home.nextSibling);
+        project.parentNode.insertBefore(footer, project.nextSibling);
+        this.$route.router.go(link);
+
+        prepareFooter.style.height = window.innerHeight + 'px';
+        prepareFooter.classList.add('prepare');
+        footer.classList.remove('go-to-next');
+        project.classList.remove('go-to-next');
+
+        setTimeout (this.resetFooter, 1900);
+      },
+      resetFooter () {
+        let prepareFooter = document.querySelectorAll('.next-project-prepare')[0],
+            project = document.querySelectorAll('.project')[0],
+            footer = document.querySelectorAll('.next-project')[0];
+
+        prepareFooter.classList.remove('prepare');
+        footer.style.height = '200px';
+        footer.style.maxHeight = '200px';
       }
     }
 });
